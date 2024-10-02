b8b9db485a3f1954cbbfcd8e7c7028ec31699c4b
amandasarita <132926235+amandasarita@users.noreply.github.com>
2024-10-02 12:05:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'divas',
+    'qual a melhor serie?',
+    'brooklyn nine-nine'
+)
+
+criaCartao(
+    'inteligentes',
+    'qual o melhor episodio da serie?',
+    'S05EP14'
+)
+
+criaCartao(
+    'FUBANGAS',
+    'qual o melhor personagem?',
+    'todos'
+)
+
+criaCartao(
+    'arraso',
+    'qual o atual presidente?',
+    'lula (uma bosta)'
+)
